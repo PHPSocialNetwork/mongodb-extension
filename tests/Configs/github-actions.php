61ec220c969e34ccb415f57e54cb57caa6adb4f0
Georges <contact@geolim4.com>
2024-01-07 09:31:31 +0100

    Fixed Mongodb build

--- tests/Configs/github-actions.php
+++ tests/Configs/github-actions.php
@@ -4,7 +4,7 @@ use Phpfastcache\Drivers\Mongodb\Config as MongodbConfig;
 
 return (fn(MongodbConfig $config) => $config->setItemDetailedDate(true)
     ->setHost('127.0.0.1')
-    ->setOptions(['authSource' => 'admin'])
+    ->setOptions(getenv('MONGODB_AUTH_SOURCE') ? ['authSource' => getenv('MONGODB_AUTH_SOURCE')] : [])
     ->setDatabaseName('pfc_test')
     ->setCollectionName('pfc_test')
     ->setUsername('test')
